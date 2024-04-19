3d79f0d4e07996c70567e84efff2cb6dad71a716
SérgioAndrade <sergiovenicio2015@gmail.com>
2024-04-19 11:34:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ dependencies = [
     # recovery, thus those versions should not be used.
     # https://github.com/googleapis/python-pubsub/issues/74
     "google-api-core[grpc]==1.14.0",
+    "google-resumable-media==1.3.3",
     "grpc-google-iam-v1==0.12.3",
     'enum34; python_version < "3.4"',
 ]
